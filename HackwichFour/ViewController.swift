415abe3c386be6d84681c7a80ba2966875bac67e
NoahNua <NoahNua@hawaii.edu>
2021-02-09 10:34:24 -1000

    completed parts 1-9

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var firstTabLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        when the app first opens up, we want to set tthe labelʻs to "About"
+        firstTabLabel.text = "About"
     }
 
 
---
+++ HackwichFour/seconedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  seconedViewController.swift
+//  HackwichFour
+//
+//  Created by Noah Nua on 2/9/21.
+//
+
+import UIKit
+
+class seconedViewController: UIViewController {
+
+    @IBOutlet weak var seconedTabLabel: UILabel!
+   
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+   
+
+}
